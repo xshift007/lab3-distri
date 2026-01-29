4d9a7ada9a25f762823f254f5e9390e439082d97
Nicolás Sánchez <nicolas.sanchez.al@usach.cl>
2026-01-29 00:52:47 -0300

    clean

--- run_load.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "--- Iniciando Carga Normal (1 evento/seg) ---"
-
-# Forzamos las variables de entorno para una carga suave
-export EVENT_RATE=1.0
-export ENABLE_BURST=false
-
-# Levantamos todo reconstruyendo por si hubo cambios
-docker compose up --build
